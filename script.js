8d758f8c29a1549886deeffc2e328bb38c437542
Mosarraf Hosain <46757936+mosarrafhosain@users.noreply.github.com>
2020-06-23 22:18:35 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+  $(function () {
+    var str = location.href.toLowerCase();
+    $('.nav li a').each(function () {
+      if (str.indexOf(this.href.toLowerCase()) > -1) {
+        $("li.active").removeClass("active");
+        $(this).parent().addClass("active");
+      }
+    });
+    $('li.active').parents().each(function () {
+      if ($(this).is('li')) {
+        $(this).addClass("active");
+      }
+    });
+  });
+</script>
